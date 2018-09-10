05aa1219e369ecf89d87f2371fee3c7191e20871
Kaustubh Kekre <kkekre90@gmail.com>
2018-09-10 09:35:11 +0530

    commit 3

--- app/src/main/java/com/falcon/theweatherapp/MainActivity.java
+++ app/src/main/java/com/falcon/theweatherapp/MainActivity.java
@@ -52,7 +52,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
         locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
         getLocation();
